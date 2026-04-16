746665544fddf2b5746f76578cbae3bc365cd316
Kolisetty Surya Mahesh <kolisettysuryamahesh@gmail.com>
2026-04-16 16:06:34 +0530

    Initial commit

---
+++ src/main/java/com/example/knowledgehub_library_management_system/KnowledgeHubLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.knowledgehub_library_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeHubLibraryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeHubLibraryManagementSystemApplication.class, args);
+    }
+
+}
