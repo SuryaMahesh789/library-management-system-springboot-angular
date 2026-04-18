0bc2d33d08300339e3190352bc68b50425705d91
Kolisetty Surya Mahesh <kolisettysuryamahesh@gmail.com>
2026-04-18 10:54:55 +0530

    Welcome Message - Entry Point of Application

--- src/main/java/com/example/knowledgehub_library_management_system/KnowledgeHubLibraryManagementSystemApplication.java
+++ src/main/java/com/example/knowledgehub_library_management_system/KnowledgeHubLibraryManagementSystemApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KnowledgeHubLibraryManagementSystemApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(KnowledgeHubLibraryManagementSystemApplication.class, args);
+        System.out.println("Main Entry point of the application");
     }
 
 }
