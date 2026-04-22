166f83d1206160b580efafdefb002759f1b98117
Kolisetty Surya Mahesh <kolisettysuryamahesh@gmail.com>
2026-04-22 10:42:03 +0530

    feat: implement user registration with role-based initialization and database setup

--- src/main/java/com/example/knowledgehub_library_management_system/KnowledgeHubLibraryManagementSystemApplication.java
+++ src/main/java/com/example/knowledgehub_library_management_system/KnowledgeHubLibraryManagementSystemApplication.java
@@ -2,7 +2,11 @@ package com.example.knowledgehub_library_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+//import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
